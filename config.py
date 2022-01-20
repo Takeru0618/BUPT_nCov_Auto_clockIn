793f31a5e008a677f8efef9f11414cf741ee0d8d
Takeru0618 <82857532+Takeru0618@users.noreply.github.com>
2022-01-20 18:19:19 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,59 +3,65 @@ FORM_URL = "https://app.bupt.edu.cn/ncov/wap/default/save"  # 填报URL
 
 
 DATA = {
-	"ismoved": "0",  # 当前地点是否与上次在同一城市
-	"jhfjrq": "",  # 计划返京时间
-	"jhfjjtgj": "",  # 计划返京交通工具
-	"jhfjhbcc": "",  # 计划返京航班班次/车次
-	"sfxk": "0",  # 未解释
-	"xkqq": "",  # 未解释
-	"szgj": "",  # 所在国家
-	"szcs": "",  # 所在地区
-	"zgfxdq": "0",  # 中高风险地区
-	"mjry": "0",  # 密切接触人员
-	"csmjry": "0",  # 近14日内本人/共同居住者是否去过疫情发生场所
-# uid: 35179
-	"date": "20201216",  # 填报日期
-	"tw": "2",  # 体温范围
-	"sfcxtz": "0",  # 今日是否出现发热，咽痛，干咳，咳痰，乏力，呕吐，腹泻，嗅觉异常，味觉异常
-	"sfyyjc": "0",  # 是否到相关医院或门诊检查
-	"jcjgqr": "0",  # 属于正常情况
-	"jcjg": "",  # 未解释
-	"sfjcbh": "0",  # 今日是否接触无症状感染/疑似/确诊人群
-	"sfcxzysx": "0",  # 是否有任何与疫情相关的， 值得注意的情况
-	"qksm": "",  # 情况说明
-	"remark": "", # 其他信息
-	# "address": "北京市海淀区北太平庄街道北京邮电大学北京邮电大学海淀校区",
-	"area": "北京市  海淀区",
-	"province": "北京市",
-	"city": "北京市",
-	"created": "1608086660",  # 时间戳
-	"sfzx": "1",  # 是否在校
-	"sfjcwhry": "0",  # 是否接触武汉人员
-	"sfcyglq": "0",  # 是否处于观察期
-	"gllx": "",  # 观察场所
-	"glksrq": "",  # 观测开始实际
-	"jcbhlx": "",  # 接触人群类型
-	"jcbhrq": "",  # 接触时间
-	"sftjwh": "0",  # 今日是否经停武汉
-	"sftjhb": "0",  # 今日是否到过或者经停湖北其他地区(除武汉)
-	"fxyy": "",  # 返校原因
-	"bztcyy": "",  # 不合前一天同城原因
-	"fjsj": "20200830",  # 返京时间
-	"sfjchbry": "0",  # 今日是否接触过近14日内在湖北其他地区（除武汉）活动过的人员
-	"sfjcqz": "",  # 未解释
-	"jcqzrq": "",  # 未解释
-	"jcwhryfs": "",  # 接触方式
-	"jchbryfs": "",  # 接触方式
-	"xjzd": "",  # 现居住地
-	"sfsfbh": "0",  # 未给出
-	"jhfjsftjwh": "0", # 未给出
-	"jhfjsftjhb": "0", # 未给出
-	"szsqsfybl": "0",  # 所在社区是否有确诊病例
-	"sfygtjzzfj": "0",  # 家中是否有共同居住者返京
-	"gtjzzfjsj": "",  # 共同居住者返京时间
-	"sfsqhzjkk": "0",  # 未给出
-	"sqhzjkkys": "",  # 未给出
+address: "黑龙江省鸡西市虎林市虎林镇北兴西街中欧丽景"
+area: "黑龙江省 鸡西市 虎林市"
+bztcyy: 4
+city: "鸡西市"
+created: 1642633534
+created_uid: 0
+csmjry: "0"
+date: "20220120"
+geo_api_info: "{\"type\":\"complete\",\"info\":\"SUCCESS\",\"status\":1,\"fEa\":\"jsonp_451240_\",\"position\":{\"Q\":45.77523,\"R\":132.9717,\"lng\":132.9717,\"lat\":45.77523},\"message\":\"Get ipLocation success.Get address success.\",\"location_type\":\"ip\",\"accuracy\":null,\"isConverted\":true,\"addressComponent\":{\"citycode\":\"0467\",\"adcode\":\"230381\",\"businessAreas\":[],\"neighborhoodType\":\"\",\"neighborhood\":\"\",\"building\":\"\",\"buildingType\":\"\",\"street\":\"北兴西街\",\"streetNumber\":\"33号\",\"country\":\"中国\",\"province\":\"黑龙江省\",\"city\":\"鸡西市\",\"district\":\"虎林市\",\"towncode\":\"230381100000\",\"township\":\"虎林镇\"},\"formattedAddress\":\"黑龙江省鸡西市虎林市虎林镇北兴西街中欧丽景\",\"roads\":[],\"crosses\":[],\"pois\":[]}"
+glksrq: ""
+gllx: ""
+gtjzzfjsj: ""
+gwszdd: ""
+id: 17012178
+ismoved: 1
+jcbhlx: ""
+jcbhrq: ""
+jchbryfs: ""
+jcjgqr: "0"
+jcqzrq: ""
+jcwhryfs: ""
+jhfjhbcc: ""
+jhfjjtgj: ""
+jhfjrq: ""
+jrsfqzfy: ""
+jrsfqzys: ""
+mjry: "0"
+province: "黑龙江省"
+qksm: ""
+remark: ""
+sfcxtz: "0"
+sfcxzysx: "0"
+sfcyglq: "0"
+sfjcbh: "0"
+sfjchbry: "0"
+sfjcqz: ""
+sfjcwhry: "0"
+sfjzdezxgym: 1
+sfjzxgym: 1
+sfsfbh: 1
+sfsqhzjkk: ""
+sftjhb: "0"
+sftjwh: "0"
+sfxk: 0
+sfygtjzzfj: 0
+sfyqjzgc: ""
+sfyyjc: "0"
+sfzx: 0
+sqhzjkkys: ""
+szcs: ""
+szgj: ""
+szsqsfybl: 0
+tw: "3"
+uid: "50656"
+xjzd: "黑龙江省虎林市"
+xkqq: ""
+xwxgymjzqk: 3
+ymjzxgqk: ""
+zgfxdq: "0"
 # created_uid: 0
 # id: 8388190
 	"gwszdd": "",  # 未给出
